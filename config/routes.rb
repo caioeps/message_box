c46024f06dceef2b8985abc62986ff79aabce703
Caio Ergos <caioergos@gmail.com>
2017-12-15 21:11:19 -0300

    Add route entries to MessagesController

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  root 'messages#index'
+
   devise_for :users,
     controllers: {
       passwords: "users/passwords",
       sessions: "users/sessions",
       registrations: "users/registrations"
     }
+
+  resources :messages, only: %i[index create new]
 end
