88177e70c2001d837efbd6683e5e5ddee496118d
Caio Ergos <caioergos@gmail.com>
2017-12-16 01:14:58 -0300

    Adjust packs/application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@
 // layout file, like app/views/layouts/application.html.erb
 
 // Support component names relative to this directory:
-var componentRequireContext = require.context("components", true, /^\.\/.*\.js$/);
+var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
 ReactRailsUJS.mountComponents()
