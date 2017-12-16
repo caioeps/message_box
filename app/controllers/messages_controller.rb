44181cd0ca02da72202f169e3083dd45ec2522a7
Caio Ergos <caioergos@gmail.com>
2017-12-16 14:45:06 -0300

    MessagesController using mailbox layout

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,21 @@
 class MessagesController < ApplicationController
+  layout 'mailbox'
+
   before_action :authenticate_user!
 
   def index
-    @messages = Message.all
+    @messages = Message.all.as_json
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @messages }
+    end
   end
 
   def create
   end
 
   def new
+    @message = Message.new
   end
 end
