8b1d48fd0417d6902535dd35cb2deacea8d3c7e0
Caio Ergos <caioergos@gmail.com>
2017-12-16 01:56:11 -0300

    Create Uuid.js
    React children need unique keys.

---
+++ app/javascript/helpers/Uuid.js
@@ -0,0 +1,12 @@
+export class Uuid {
+  static generate() {
+    return this.template().replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 3 | 8);
+      return v.toString(16);
+    });
+  }
+
+  static template() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
+  }
+}
