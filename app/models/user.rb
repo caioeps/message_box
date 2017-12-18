e0f17a97d476a794944b6dfa8a74fc09f9199f3c
Caio Ergos <caioergos@gmail.com>
2017-12-18 20:25:26 -0300

    user model now acts as token authenticable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  acts_as_token_authenticatable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
