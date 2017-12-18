b076dc62be4dc2765f3ee8fc4886782dca3654d9
Caio Ergos <caioergos@gmail.com>
2017-12-18 20:52:16 -0300

    Add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'messages/index'
+  end
+
+  namespace :api do
+    get 'messages/create'
+  end
+
   root 'messages#index'
 
   devise_for :users,
@@ -7,6 +15,10 @@ Rails.application.routes.draw do
       sessions: "users/sessions",
       registrations: "users/registrations"
     }
+
+  namespace :api do
+    resources :messages, only: %i[index create new show], defaults: { format: :json }
+  end
 
   namespace :messages do
     resources :archives, only: %i[index]
