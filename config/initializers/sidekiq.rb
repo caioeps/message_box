a88a29d64d842be555e119ee947b41c772e34e4a
Caio Ergos <caioergos@gmail.com>
2018-12-04 23:59:51 -0300

    still adjusting redis conn pool

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 # three pumas = 3 connections
 Sidekiq.configure_client do |config|
-  config.redis = { size: 1 }
+  config.redis = { size: 2 }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { size: 1 }
+  config.redis = { size: 2 }
 end
