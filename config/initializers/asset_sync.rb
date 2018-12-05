7c6207a4a1af4657b7e04812687ed8e721199173
Caio Ergos <caioergos@gmail.com>
2018-12-04 21:44:02 -0300

    trying to make asset sync work

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -40,7 +40,7 @@ if defined?(AssetSync)
     # config.include_manifest = false
     #
     # Fail silently.  Useful for environments such as Heroku
-    # config.fail_silently = true
+    config.fail_silently = true
     #
     # Log silently. Default is `true`. But you can set it to false if more logging message are preferred.
     # Logging messages are sent to `STDOUT` when `log_silently` is falsy
